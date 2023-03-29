6c727135a4f12adfbeba85665712b30a6cbc7f4c
ozgengin <ozgeenginnn@gmail.com>
2023-03-29 12:46:32 +0300

    seed data

--- src/Infrastructure/Data/ShopContext.cs
+++ src/Infrastructure/Data/ShopContext.cs
@@ -22,6 +22,7 @@ namespace Infrastructure.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly()); //Mevcut projedeki confg. oku
+             
         }
     }
 }
