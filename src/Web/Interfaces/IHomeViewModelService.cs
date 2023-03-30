d1c8fdc006e9d29ca99881d63ad982ee9484c3cb
ozgengin <ozgeenginnn@gmail.com>
2023-03-30 12:33:05 +0300

    homeviewmodel

---
+++ src/Web/Interfaces/IHomeViewModelService.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Web.Interfaces
+{
+    public interface IHomeViewModelService
+    {
+        Task<HomeViewModel> GetHomeViewModelAsync(int? categoryId, int? brandId, int pageId = 1);
+    }
+}
